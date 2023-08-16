32e659f1e2884f8de7ca07b2f7763633d6674f4d
Daniel Gibim <DanielGibimdev@gmail.com>
2023-08-16 11:24:51 -0700

    .

--- Calculo_Montante/src/Main.java
+++ Calculo_Montante/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+        double capital, juros, montante;
+        int tempo;
+
+        System.out.println("Digite o capital aplicado:");
+        capital = sc.nextDouble();
+        System.out.println("Digite a % de juros:");
+        juros = sc.nextDouble();
+        System.out.println("Digite o tempo de aplicação:");
+        tempo = sc.nextInt();
+        montante = capital * Math.pow((1+juros),tempo);
+        System.out.printf("O montante acumulado foi de %.2f.",montante);
+
     }
 }
