f6130a828f906299a9f33c27856e5ef3a4ffba4b
Daniel Gibim <DanielGibimdev@gmail.com>
2023-08-29 17:26:21 -0700

    Ex Notas

--- Exercicio_Notas/src/Main.java
+++ Exercicio_Notas/src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        final int TAM = 5;
+        double [] notas = new double[TAM];
+
+        Scanner sc = new Scanner(System.in);
+        double soma = 0;
+
+        for (int i = 0; i < TAM; i ++ ) {
+
+            System.out.println("Digite a sua nota: " + (i+1) + ":");
+            notas[i] = sc.nextDouble();
+            soma = soma + notas[i];
+
+        }
+        double media = soma / TAM;
+        System.out.println("Média:" + media);
+
+        for (int i = 0; i < TAM; i ++){
+            if (notas[i] > media)
+                System.out.println("A nota " + (i+1) + " sendo " + notas[i] + " foi maior que a média.");
+
+        }
+
     }
 }
