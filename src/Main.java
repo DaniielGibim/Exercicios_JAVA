046006b4748097162713e5a41940eb4ef5ab6792
Daniel Gibim <DanielGibimdev@gmail.com>
2024-03-04 05:51:43 -0800

    .

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite a quantidade de minutos utilizados: ");
+        int minutos = sc.nextInt();
+        double conta = 50;
+        if (minutos > 100) {
+            conta = conta + (minutos - 100) * 2;
+        }
+        System.out.printf("O valor da conta a ser paga é = R$ %.2f%n", conta);
+        sc.close();
     }
 }
